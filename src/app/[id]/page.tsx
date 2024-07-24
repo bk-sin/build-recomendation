1a18e72e383fc7b74727dabac064814b97c7605a
Emiliano Alegre <emiliano.alegre@vortex-it.com>
2024-07-24 17:28:04 -0300

    option colors

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -9,11 +9,12 @@ export async function generateStaticParams() {
 }
 
 async function getData({ id }: { id: string }) {
-  return data.questions.find((question) => +question.id === +id);
+  return data.questions.find((question) => question.id === id);
 }
 
 export default async function Question({ params }: { params: { id: string } }) {
   const question = await getData({ id: params.id });
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-20">
       <h1>{question?.text}</h1>
@@ -22,8 +23,13 @@ export default async function Question({ params }: { params: { id: string } }) {
           <a
             key={idx}
             href={option.nextQuestionId?.toString()}
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            className={`group rounded-lg border-[15px] border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:${
+              option?.color || "border-green-500"
+            } hover:dark:bg-neutral-800/30`}
             rel="noopener noreferrer"
+            style={{
+              borderStyle: "groove",
+            }}
           >
             <h2 className="mb-3 text-2xl font-semibold text-center">
               {option.text}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,27 @@
+import data from "../../data.json";
+
 export default function Home() {
+  console.log(data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Build Recomendation</h1>
-      <h2>Whats u want to play?</h2>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-20">
+      <h1>3.25 Build Recomendation</h1>
+      <h2>Choose your shit</h2>
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold text-center">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold text-center">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold text-center">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold text-center">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
+        {data.questions[0]?.options.map((option, idx) => (
+          <a
+            key={idx}
+            href={option.nextQuestionId?.toString()}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            rel="noopener noreferrer"
+          >
+            <h2 className="mb-3 text-2xl font-semibold text-center">
+              {option.text}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+          </a>
+        ))}
       </div>
     </main>
   );
